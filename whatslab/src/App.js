496dde35d53e82c968e513a17faa63ea0e8d9ab8
GuilhermeSoutello <guisoutello@gmail.com>
2021-05-07 14:41:30 -0300

    versao propria

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const ContainerPrincipal = styled.div`
+
+`
+const ContainerDeInput = styled.div`
+
+`
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <ContainerPrincipal>
+      <ContainerDeInput>
+        <input></input>
+        <input></input>
+        <button>Enviar</button>
+      </ContainerDeInput>
+      <p>Mensagens enviadas</p>
+      </ContainerPrincipal>
     </div>
   );
 }
